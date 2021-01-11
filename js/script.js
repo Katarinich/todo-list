9131f983b6ac594cbfbbab25a01590983e7fbbed
Katarinich <katarina.chirich@gmail.com>
2021-01-11 20:35:40 +0300

    added functionality to delete all checked tasks

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 import addTask from './task-operations/add-task.js';
+import deleteCheckedTasks from './task-operations/delete-checked-tasks.js';
 
 // находим форму добавления
 const addForm = document.querySelector('.add-form > form');
+const deleteCheckedBtn = document.querySelector('.delete-checked-btn');
 
 // вешаем обработчик события submit (отправки) на форму
 addForm.addEventListener('submit', addTask);
+deleteCheckedBtn.addEventListener('click', deleteCheckedTasks);
