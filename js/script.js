efb3b699f67dff7b534d9a02d2220e4783290bbc
Katarinich <katarina.chirich@gmail.com>
2021-01-14 21:21:35 +0300

    Implemented TaskList

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-import addTask from './task-operations/add-task.js';
+import taskList from './tasks.js';
+
+import addTask, { createTask } from './task-operations/add-task.js';
 import deleteCheckedTasks from './task-operations/delete-checked-tasks.js';
 
 // находим форму добавления
@@ -8,3 +10,7 @@ const deleteCheckedBtn = document.querySelector('.delete-checked-btn');
 // вешаем обработчик события submit (отправки) на форму
 addForm.addEventListener('submit', addTask);
 deleteCheckedBtn.addEventListener('click', deleteCheckedTasks);
+
+taskList.tasks.forEach((task) => {
+  createTask(task);
+});
