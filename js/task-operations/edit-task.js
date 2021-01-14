c9c30381eca0efda2e796ecb466f16ce17f51e80
Katarinich <katarina.chirich@gmail.com>
2021-01-14 19:24:04 +0300

    Finish edit functionality

--- js/task-operations/edit-task.js
+++ js/task-operations/edit-task.js
@@ -1,3 +1,32 @@
+function submitTask(event) {
+  if (event.keyCode !== 13) {
+    return;
+  }
+
+  const li = event.target.closest('li');
+
+  const icon = li.querySelector('.edit-btn i');
+
+  const checkbox = li.querySelector('input[type="checkbox"]');
+
+  saveTask(li, icon, checkbox);
+}
+
+function saveTask(li, icon, checkbox) {
+  const input = li.querySelector('input[type="text"]');
+  const { value: newText } = input;
+
+  const newSpan = document.createElement('span');
+  newSpan.textContent = newText;
+
+  li.replaceChild(newSpan, input);
+
+  icon.classList.remove('fa-save');
+  icon.classList.add('fa-edit');
+
+  checkbox.disabled = false;
+}
+
 function editTask(event) {
   /* 
     находим span текущего task
@@ -12,25 +41,35 @@ function editTask(event) {
 
   const span = li.querySelector('span');
 
+  const icon = li.querySelector('.edit-btn i');
+
+  const checkbox = li.querySelector('input[type="checkbox"]');
+
   if (span) {
     const { textContent: text } = span;
 
     const input = document.createElement('input');
-    input.setAttribute('value', text);
+    
     input.setAttribute('type', 'text');
 
+    input.addEventListener('keydown', submitTask);
+
     li.replaceChild(input, span);
+
+    input.focus();
+    // чтобы курсор был в конце при фокусе
+    input.value = '';
+    input.value = text;
+
+    icon.classList.remove('fa-edit');
+    icon.classList.add('fa-save');
+
+    checkbox.disabled = true;
 
     return;
   }
 
-  const input = li.querySelector('input[type="text"]');
-  const { value: newText } = input;
-
-  const newSpan = document.createElement('span');
-  newSpan.textContent = newText;
-
-  li.replaceChild(newSpan, input);
+  saveTask(li, icon, checkbox);
 }
 
 export default editTask;
